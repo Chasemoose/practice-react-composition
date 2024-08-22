101e59446045da588056e233a70929e6845e46e2
Jaro <jaroswidnik@interia.pl>
2024-08-22 13:56:54 +0200

    Finished ex2

--- 02/Form.js
+++ 02/Form.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 class Form extends React.Component {
+
+    refInput = React.createRef()
+
+    handleSubmit = (e) => {
+
+        e.preventDefault()
+               
+        this.props.addItem(this.refInput.current.value)
+        this.refInput.current.value = ''
+    }
+
     render() {
         return (
-            <section>
-                <input /><input type="submit" />
-            </section>
+            <form onSubmit={this.handleSubmit}>
+                <input ref={ this.refInput } /><input type="submit" />
+            </form>
         )
     }
 }
